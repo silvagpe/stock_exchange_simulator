620886b738013f979e7896310f45a1088f040a27
Rafael Da Silva <silvagpe@gmail.com>
2024-06-19 17:07:01 -0300

    testfixure

--- src/server/MatchEngine/IOrderBookService.cs
+++ src/server/MatchEngine/IOrderBookService.cs
@@ -1,9 +1,11 @@
-﻿using Common.Entities;
+﻿using System.Collections.Concurrent;
+using Common.Entities;
 
 namespace server.MatchEngine
 {
     public interface IOrderBookService
     {
         void AddOrder(Order order);
+        ConcurrentDictionary<decimal, ConcurrentQueue<Order>>? GetOrdersBySymbol(Side side, string symbol);
     }
 }
--- tests/UnitTest.Server/TestFixture.cs
+++ tests/UnitTest.Server/TestFixture.cs
@@ -1,6 +1,30 @@
-﻿namespace UnitTest.Server;
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using Moq.AutoMock;
+using server.MatchEngine;
+
+namespace UnitTest.Server;
 
 public class TestFixture
 {
+    private readonly AutoMocker _autoMocker;
 
+    public TestFixture()
+    {
+        _autoMocker = new AutoMocker();   
+
+        ConfigureOrderBookService();
+    }
+
+    private void ConfigureOrderBookService(){
+        var log = new Mock<ILogger<OrderBookService>>();
+        var bookService = new OrderBookService(log.Object);
+        _autoMocker.Use<IOrderBookService>(bookService);
+    }
+
+    public T CreateInstance<T>() where T : class => _autoMocker.CreateInstance<T>();
+
+    public Mock<T> GetMock<T>() where T : class => _autoMocker.GetMock<T>();
+
+    public void Use<T>(T value) where T : class => _autoMocker.Use<T>(value);
 }
