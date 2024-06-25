2de2d428bcf26a859ee93373841d60edd4c173d8
Rafael Da Silva <silvagpe@gmail.com>
2024-06-24 22:13:47 -0300

    counterpart

--- src/server/MatchEngine/ProcessOrders/ProcessOrderBySymbol.cs
+++ src/server/MatchEngine/ProcessOrders/ProcessOrderBySymbol.cs
@@ -5,10 +5,15 @@ namespace server.MatchEngine.ProcessOrders;
 
 public class ProcessOrderService
 {
-    public void Process(Order customerOder, ConcurrentDictionary<string, ConcurrentDictionary<decimal, ConcurrentQueue<Order>>> orders){
+    public void ProcessLimitOrders(Order customerOder, ConcurrentDictionary<string, ConcurrentDictionary<decimal, ConcurrentQueue<Order>>> counterpartOrders){
 
         if (customerOder is null) return;
+        if (customerOder.OrdType != OrdType.LIMIT) return;
 
+        var price = customerOder.Price;
+        var symbol = customerOder.Symbol;
+        var orders = counterpartOrders.GetOrdersByPrice(symbol ,price);
+        if (orders is null) return;
                 
 
         // customerOder.Side
